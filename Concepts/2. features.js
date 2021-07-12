8faaeb453a8c9744eff50a41294a7e294e603ec5
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-12 18:54:45 +0530

    Update 2. features.js

--- Concepts/2. features.js
+++ Concepts/2. features.js
@@ -86,3 +86,17 @@
 
   in the example above _base.scss, here '_' will tell compiler to ignore that file.
   when we call that file with @use then only it will get compiled.
+  
+  
+  
+  
+  there are two ways to import a module
+  1. @import
+  2. @use
+
+  1. @import  -  it will import everything from target file and we can use anything from the target file directly. so it becomes difficult to backtrack from which file we are 
+                 referencing that code (variable, mixins)
+
+  2. @use  - it will also import everything from target file but to access that we need to call it as namespace.variable_name
+             at line 82, we have called it as base.$primary-color
+             so it makes easier to understand from which file we are referencing that particular variable.
