5bdbebf9380e6ac1bb94cd98e3fd8a41adff19cc
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-13 20:13:57 +0530

    Update 1. Intro.js

--- Concepts/1. Intro.js
+++ Concepts/1. Intro.js
@@ -4,3 +4,23 @@ Sass - syntactically awesome style sheets
 It is a CSS preprocessor which allows us to use advanced features that CSS natively do not supports.
 
 sass files has .scss exention.
+
+
+preprocessor - is a software program which will take the scss/less code as a input & convert that into native css file.
+
+// Setup of scss
+
+we need to install sass to work with scss
+    - npm install sass
+
+
+    
+to compile SCSS file to CSS file
+    - sass  target_file.scss   destination_file.css
+
+
+
+to watch changes in SCSS file & compile automatically
+    - sass  --watch  scss/style.scss  css/style.css
+
+here, --watch flag will watch for changes on style.scss & will automatically compile to new style.css file
